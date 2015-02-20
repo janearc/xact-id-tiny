d7926282742c649d84fd41740e14c5f907920243
jane arc <jane@cpan.org>
2015-02-20 10:56:49 -0500

    rev bump, adds get-by-id

--- test/03-get.js
+++ test/03-get.js
@@ -3,13 +3,15 @@ var assert = require( 'assert' )
 
 xit.start();
 
-var xact = new xit.xact();
-xit.add( xact );
-xit.end( xact );
-
 it( 'retrieve xact', function () {
+	var xact = new xit.xact();
+	xit.add( xact );
+	xit.end( xact );
 	assert( typeof xit.get( xact ) != 'Error' );
 } )
 it( 'retrieve xact-by-id', function () {
+	var xact = new xit.xact();
+	xit.add( xact );
+	xit.end( xact );
 	assert( typeof xit.get( xact.serial ) != 'Error' );
 } );
